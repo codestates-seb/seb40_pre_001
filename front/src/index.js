4342e6630c38e8c76da9462d308d5c3241c741e6
inside-eun <dameun0808@gmail.com>
2022-10-25 01:01:51 +0900

    🖋Feat : 초기 설정 완료

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,24 @@
+/* eslint-disable */
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
+import {worker} from './mocks/broswer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
+const queryClient = new QueryClient();
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
+  </>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
