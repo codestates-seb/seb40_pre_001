4d63dae20cbb40ceb092c216f521f5bcc4ed8da1
yhwang15 <donkassu@gmail.com>
2022-10-25 10:43:52 +0900

    backend 초기셋팅

---
+++ back/src/main/java/team001_be/stackoverflowCloneDemo/StackoverflowCloneDemoApplication.java
@@ -0,0 +1,13 @@
+package team001_be.stackoverflowCloneDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowCloneDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowCloneDemoApplication.class, args);
+	}
+
+}
