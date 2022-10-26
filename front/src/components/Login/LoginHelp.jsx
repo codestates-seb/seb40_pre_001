e43e95f5a0ba8028c33b199427f42c2d89f93b3d
joewontae <tyuiop3762@naver.com>
2022-10-26 13:50:49 +0900

    ✒️Feat: 로그인 도움말 컴포넌트 추가

---
+++ front/src/components/Login/LoginHelp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as S from "./LoginHelp.style"
+const LoginHelp = () => {
+    return (
+        <>
+            <S.HelpContainer>
+                <S.HelpContent>Don’t have an account?</S.HelpContent>
+                <S.HelpLink>Sing up</S.HelpLink>
+            </S.HelpContainer>
+            <S.HelpContainer>
+                <S.HelpContent>Are you an employer?</S.HelpContent>
+                <S.HelpLink>Sign up on Talent</S.HelpLink>
+            </S.HelpContainer>
+        </>
+    )
+}
+
+export default LoginHelp;
---
+++ front/src/components/Login/LoginHelp.style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+const HelpContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+width: 255px;
+margin: 12px 6px 12px 6px;
+`
+
+const HelpContent = styled.div`
+ margin-right: 10px;
+ font-size: 13px;
+`
+
+const HelpLink = styled.a`
+color: #0a95ff;
+`
+
+
+export { HelpContainer, HelpContent, HelpLink }
