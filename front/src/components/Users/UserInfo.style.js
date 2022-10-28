01a59677aebe4daa1af44f47e61e2e0ea28704c1
joewontae <tyuiop3762@naver.com>
2022-10-28 21:49:24 +0900

    ✒️Feat: 유저페이지 추가

--- front/src/components/Users/UserInfo.style.js
+++ front/src/components/Users/UserInfo.style.js
@@ -3,11 +3,9 @@ import styled from "styled-components";
 
 const UserInfoContainer = styled.div`
 background-color: white;
-/* display: flex; */
 width: 253.75px;
 height: 87.18px;
 padding: 5px 6px 7px 7px;
-float: left;
 `
 
 const UserDetails = styled.div`
@@ -15,7 +13,6 @@ display: inline-block;
 width: 176.75px;
 height:54.09px;
 margin: 0 0 0 9px;
-background-color: gray;
 line-height: 1.3;
 `
 const UserInfoProfil = styled.img`
@@ -24,20 +21,17 @@ height: 48px;
 `
 const UserInfoName = styled.a`
 font-size: 15px;
-background-color: red;
 display: block;
 `
 const UserInfoLocation = styled.span`
 font-size: 12px;
 margin: 0 0 2px;
-background-color: beige;
 display: block;
 `
 const UserInfoScore = styled.span`
 font-size: 12px;
 font-weight: 300;
 margin: 0 0 4px;
-background-color: aqua;
 display: block;
 `
 const UsersTagsContainer = styled.div`
--- front/src/pages/Users/Users.jsx
+++ front/src/pages/Users/Users.jsx
@@ -1,8 +1,54 @@
 import React from "react";
+import UserInfo from "../../components/Users/UserInfo";
+import * as S from "./Users.style.js"
+import Title from "../../components/Title/Title"
+import UsersFilter from "../../components/UsersFilter/UsersFilter";
 
 const Users = () => {
     return (
-        <>users 페이지 </>
+        <S.ContentContainer>
+            <Title></Title>
+            <UsersFilter />
+            <S.UsersContainer>
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+                <UserInfo />
+
+            </S.UsersContainer>
+        </S.ContentContainer>
     )
 }
 
