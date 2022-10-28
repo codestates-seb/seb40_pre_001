cae3c59490c99a41db94642607278f809dbede90
joewontae <tyuiop3762@naver.com>
2022-10-28 11:40:00 +0900

    ✒️Feat: 유저페이지

---
+++ front/src/pages/Users/Users.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const UsersContainer = styled.div`
+width: 1051px;
+height: 863.41px;
+background-color: red;
+`
---
+++ front/src/pages/Users/Users.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Users = () => {
+    return (
+        <>users 페이지 </>
+    )
+}
+
+export default Users
