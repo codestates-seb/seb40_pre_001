f4fbd7b676dde854d779f6a5ee129fc6aa087222
joewontae <tyuiop3762@naver.com>
2022-10-31 18:34:25 +0900

    ✒️Feat:유저 페이지 레이아웃 구현

--- front/src/pages/Users/Users.jsx
+++ front/src/pages/Users/Users.jsx
@@ -1,55 +1,32 @@
-import React from "react";
-import UserInfo from "../../components/Users/UserInfo";
-import * as S from "./Users.style.js"
-import Title from "../../components/Title/Title"
-import UsersFilter from "../../components/UsersFilter/UsersFilter";
+import React from 'react';
+import UserInfo from '../../components/Users/UserInfo';
+import * as S from './Users.style.js';
+import Title from '../../components/Title/Title';
+import UsersTab from '../../components/UsersTab/UsersTab';
+import UsersFilter from '../../components/UsersFilter/UsersFilter';
+import UsersPagiNation from '../../components/Questions/Main/UsersPaginaton/UsersPagination';
+import UserData from '../../components/Users/UserData';
 
 const Users = () => {
-    return (
-        <S.ContentContainer>
-            <Title></Title>
-            <UsersFilter />
-            <S.UsersContainer>
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
-                <UserInfo />
+  return (
+    <S.Container>
+      <Title></Title>
+      <UsersFilter />
+      <UsersTab />
+      <S.UsersContainer>
+        {UserData.map((UserData) => {
+          return <UserInfo key={name} UserData={UserData} />;
+        })}
+        <UserInfo />
+      </S.UsersContainer>
+      <S.UsersfooterContainer>
+        <S.UsersLegend>
+          weekly / monthly / quarterly resutatiom leagues
+        </S.UsersLegend>
+        <UsersPagiNation />
+      </S.UsersfooterContainer>
+    </S.Container>
+  );
+};
 
-            </S.UsersContainer>
-        </S.ContentContainer>
-    )
-}
-
-export default Users
+export default Users;
