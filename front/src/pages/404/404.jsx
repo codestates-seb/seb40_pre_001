04168e520ed03b4d134effdba625c5d1ce50ce4a
leezer94 <2kunhee94@gmail.com>
2022-11-02 10:56:10 +0900

    ✒️Feat: 404 페이지 구성 추후 라우팅 수정 예정

---
+++ front/src/pages/404/404.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NotFoundIcon } from '../../components/@common/Icons';
+import { SmallBlueSpan } from '../../components/@common/Text/Text.style';
+import * as S from './404.style';
+
+const NotFound = () => {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.IconContainer>
+          <NotFoundIcon />
+        </S.IconContainer>
+        <S.Content>
+          <S.Header>Page not found</S.Header>
+          <S.SubHeading>{`We're sorry, we couldn't find the page you requested.`}</S.SubHeading>
+
+          <S.ContentBody>
+            <p>
+              Try <SmallBlueSpan>searching for similar questions</SmallBlueSpan>
+            </p>
+
+            <p>
+              Browse our <SmallBlueSpan>recent questions</SmallBlueSpan>
+            </p>
+            <p>
+              Browse our <SmallBlueSpan>popular tags</SmallBlueSpan>
+            </p>
+
+            <p>
+              If you feel something is missing that should be here,{' '}
+              <SmallBlueSpan>contact us</SmallBlueSpan>.
+            </p>
+          </S.ContentBody>
+        </S.Content>
+      </S.Container>
+    </S.Wrapper>
+  );
+};
+
+export default NotFound;
