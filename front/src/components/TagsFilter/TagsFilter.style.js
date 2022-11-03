2a39b94937fe0f5fc152b843f80b40aae88feb38
joewontae <tyuiop3762@naver.com>
2022-11-04 00:45:02 +0900

    ✒️Feat:태그필터 컴포넌트 구현

---
+++ front/src/components/TagsFilter/TagsFilter.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TagsFilterBoxContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: stretch !important;
+  margin-bottom: 12px;
+`;
+export { TagsFilterBoxContainer };
