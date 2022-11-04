f10a021af4c72c323ba085dbc1fb86b43d38b26b
joewontae <tyuiop3762@naver.com>
2022-11-04 15:24:04 +0900

    ✒️Feat:유저커뮤니티 컴포넌트 추가

--- front/src/components/UserCommunities/UserCommunities.style.js
+++ front/src/components/UserCommunities/UserCommunities.style.js
@@ -2,9 +2,25 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
-  border: 1px solid rgba(0, 0, 0, 1);
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid rgba(0, 0, 0, 0.2);
   border-radius: 5px;
-  padding: 12px;
+  padding: 13px;
+  width: 22.5%;
+  margin-top: 10px;
 `;
 
-export { Container };
+const CommutnitiesTitle = styled.div`
+  color: blue;
+  font-size: 13px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CommutnitiesCount = styled.div`
+  color: gray;
+`;
+
+export { Container, CommutnitiesCount, CommutnitiesTitle };
