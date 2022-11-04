3c76563ea115811a4af54d5a1393e0c7dd767046
joewontae <tyuiop3762@naver.com>
2022-11-04 11:12:04 +0900

    ✒️Feat:유저 코멘티스 컴포넌트 구현

---
+++ front/src/components/UserCommunities/UserCommunities.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './UserCommunities.style';
+const UserCommunities = () => {
+  return <S.Container>Communities</S.Container>;
+};
+
+export default UserCommunities;
---
+++ front/src/components/UserCommunities/UserCommunities.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  border: 1px solid rgba(0, 0, 0, 1);
+  border-radius: 5px;
+  padding: 12px;
+`;
+
+export { Container };
