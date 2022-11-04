afa76d2200da8c22011e48b79341432496a0acc3
joewontae <tyuiop3762@naver.com>
2022-11-04 11:13:05 +0900

    ✒️Feat:유저스탯박스 컴포넌트 구현

--- front/src/components/UserStatusBox/UserStatsBox.jsx
+++ front/src/components/UserStatusBox/UserStatsBox.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
-// import * as S from "./UserStatsBox.stlye"
+import * as S from './UserStatsBox.stlye';
+
 const UserStatsBox = () => {
-  return <></>;
+  return (
+    <S.Container>
+      <S.StatsBox>
+        <S.StatsBoxContentsCount>
+          1<S.StatsBoxContentsItem>asdasd</S.StatsBoxContentsItem>
+        </S.StatsBoxContentsCount>
+        <S.StatsBoxContentsCount>
+          1<S.StatsBoxContentsItem>asdasd</S.StatsBoxContentsItem>
+        </S.StatsBoxContentsCount>
+        <S.StatsBoxContentsCount>
+          1<S.StatsBoxContentsItem>asdasd</S.StatsBoxContentsItem>
+        </S.StatsBoxContentsCount>
+        <S.StatsBoxContentsCount>
+          1<S.StatsBoxContentsItem>asdasd</S.StatsBoxContentsItem>
+        </S.StatsBoxContentsCount>
+      </S.StatsBox>
+    </S.Container>
+  );
 };
 
 export default UserStatsBox;
