fbe4ccfebd5debdaabcc572e6196a0c68f81f782
joewontae <tyuiop3762@naver.com>
2022-11-04 22:42:27 +0900

    ✒️Feat:유저페이지컨텐츠 컴포넌트 추가

---
+++ front/src/components/UserProfile/UserAnswersContent/UsersAnswersContent.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 75%;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+  margin-left: 260px;
+  padding: 12px;
+  display: grid;
+  grid-template-columns: 0.2fr 1fr 0.1fr;
+  gap: 10px;
+  align-items: center;
+`;
+
+export { Container };
