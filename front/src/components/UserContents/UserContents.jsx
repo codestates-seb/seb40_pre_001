0cbe4157cfc98b2f65c0500a516be24f19655f8d
joewontae <tyuiop3762@naver.com>
2022-11-04 11:12:38 +0900

    ✒️Feat:유저 콘텐츠 컴포넌트 구현

---
+++ front/src/components/UserContents/UserContents.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import * as S from './UserContents.stlye';
+const UserContents = () => {
+  return <S.Container>asdasd</S.Container>;
+};
+
+export default UserContents;
---
+++ front/src/components/UserContents/UserContents.stlye.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: red;
+`;
+
+export { Container };
