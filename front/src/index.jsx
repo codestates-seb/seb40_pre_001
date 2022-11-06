1fefb4f0433b834ceb6047c2206ec0a00d37c4a0
joewontae <tyuiop3762@naver.com>
2022-11-07 00:52:41 +0900

    🛠Fix:충돌오류 해결

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 // import { worker } from './mocks/broswer';
 import { GlobalStyles, theme } from './styles';
 import App from './App';
