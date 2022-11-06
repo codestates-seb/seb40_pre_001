93efc174ae7fb5733a21ab2cf7776f8c6276f5fe
joewontae <tyuiop3762@naver.com>
2022-11-07 00:03:44 +0900

    ✒️Feat: json 파일 추가

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-import { worker } from './mocks/broswer';
+// import { worker } from './mocks/broswer';
 import { GlobalStyles, theme } from './styles';
 import App from './App';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start();
-}
+// if (process.env.NODE_ENV === 'development') {
+//   worker.start();
+// }
 
 // react-query
 const queryClient = new QueryClient({
