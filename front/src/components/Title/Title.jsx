0a56b4fa2802b79affc3cfc66d1450768da45a81
leezer94 <2kunhee94@gmail.com>
2022-11-07 04:19:06 +0900

    🧺Remove: 사용되지 않는 파일 삭제\

--- front/src/components/Title/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as S from './Title.style';
-
-const Title = ({ title }) => {
-  return <S.TitleH1>{title}</S.TitleH1>;
-};
-
-export default Title;
--- front/src/components/Title/Title.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const TitleH1 = styled.h1`
-  font-weight: 400;
-  font-size: 27px;
-  margin: 0 0 24px;
-`;
-
-export { TitleH1 };
