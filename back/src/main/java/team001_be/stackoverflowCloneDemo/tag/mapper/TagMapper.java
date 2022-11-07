5e4e0ebc5f9a61268da07b5bb0ade315d600533a
you88311@naver.com <you88311@naver.com>
2022-11-07 11:12:33 +0900

    back 삭제

--- back/src/main/java/team001_be/stackoverflowCloneDemo/tag/mapper/TagMapper.java
+++
@@ -1,7 +0,0 @@
-package team001_be.stackoverflowCloneDemo.tag.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface TagMapper {
-}
--- back/src/main/java/team001_be/stackoverflowCloneDemo/tag/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package team001_be.stackoverflowCloneDemo.tag.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team001_be.stackoverflowCloneDemo.tag.entity.Tag;
-
-public interface TagRepository extends JpaRepository<Tag, Long> {
-}
